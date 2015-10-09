ba38cb5c73f880a9b4d47a2dc52e4ce857f52ad6
gvop <guusvanooijen@gmail.com>
2015-10-09 14:15:32 +0100

    got the questions reacting on the input, only in console.log

--- js/app.js
+++ js/app.js
@@ -1,30 +1,37 @@
 window.onload = function(){
 
   var guessWho = {
-    characters    :   list.characters,
-    board         :   $("#board"),
+    characters      :   list.characters,
+    board           :   $("#board"),
+    askQuestion     :   function(){
+          var features  = $("input:radio[name='features']:checked").val();
+          var adjective = $("input:radio[name='adjective']:checked").val();
+          guessWho.featuresCheck(features,adjective);
+        }
   };
+
+  $("#ask").on("click", guessWho.askQuestion)
 
   guessWho.printBoard = function(){
     for(var i=0;i<this.characters.length;i++){
        this.board.append("<li class='character'>" + this.characters[i].name + "</li>");
     };
   };
+
   guessWho.printBoard();
+
+  guessWho.featuresCheck = function(value, choice){
+    for(var i=0;i<this.characters.length;i++){
+      for(var j=0;j<this.characters[i][value].length;j++){
+        if(this.characters[i][value][j] === choice ){
+          console.log("Check!");
+        };
+      };
+    };
+  };
 };
 
-// guessWho.featuresCheck = function(value, choice){
-//   for(var i=0;i<this.charachter.length;i++){
-//     for(var j=0;j<this.charachter[i][value].length;j++){
-//       if(this.charachter[i][value][j] === choice){
-//         var name = this.charachter[i]["name"];
-//         document.getElementById(name).style.display = 'none';
-//       };
-//     };
-//   };
-// };
 
-// guessWho.featuresCheck("gender", "man");
 
 
 
