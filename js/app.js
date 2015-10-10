6398dfb6e4747960f8276147e46dfc45cfc9645f
gvop <guusvanooijen@gmail.com>
2015-10-10 11:35:16 +0100

    Player can now choose character for computer

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 window.onload = function(){
+  // $("#question-box").hide();  
+  // $("#start").hide();
 
   //Object
   var guessWho = {
     characters            :   list.characters,
+    startMessage          :   $("#game-start"),
     board                 :   $("#board"),
+    playerChoose          :   "",
     selectedCharacter     :   {},
+    askButton             :   {},
     askQuestion           :   function(){
                               this.features   =   $("input:radio[name='features']:checked").val();
                               this.adjective  =   $("input:radio[name='adjective']:checked").val();
@@ -13,20 +18,53 @@ window.onload = function(){
         }
   };
 
-  $("#ask").on("click", guessWho.askQuestion)
+  
+
+  // guessWho.setup = function(){
+  //   for(var i=0;i<this.characters.length;i++){
+  //      this.board.append("<li id='" + this.characters[i].name + "' class='character'>" + this.characters[i].name + "</li>");
+  //   } 
+
+  //   $("li").on("click", function(){
+  //     var choice = this.id; 
+  //     console.log(choice);
+  //     var array = $("li");
+  //     $("li").fadeOut("slow");
+
+  //     setTimeout(function(){
+  //         setTimeout(function(){
+  //               $("#" + choice).fadeIn("slow");
+  //               $("#start-title").html("You have choosen " + choice +"!");
+  //           }, 500);
+
+  //       },500)
+  //     })
+  // }
+
 
   //Get the board show
   guessWho.printBoard = function(){
 
-    var selected = this.characters[Math.floor(Math.random()*24)]
-    this.selectedCharacter = selected 
+    console.log(this)
 
-    console.log(this.selectedCharacter)
-    console.log(this.selectedCharacter.name)
+    var selected = this.characters[Math.floor(Math.random()*24)]
+    this.selectedCharacter = selected
+
+    console.log(this.selectedCharacter["name"][0]); //TO SEE DURING TEST WHOM THE PERSON IS
 
     for(var i=0;i<this.characters.length;i++){
        this.board.append("<li id='" + this.characters[i].name + "' class='character'>" + this.characters[i].name + "</li>");
-    };
+    }
+
+      $(".character").on("click", function(){
+      if(this.id ===guessWho.selectedCharacter["name"][0]){
+        console.log("Winner!");
+      }else{
+        guessWho.numberOfGuesses++;
+        console.log("Not the right person!");
+      };
+    });
+
   };
 
   //Checker 
@@ -36,7 +74,6 @@ window.onload = function(){
 
   guessWho.dropDowns = function(value, choice){
     for(var i=0;i<this.characters.length;i++){
-
       for(var j=0;j<this.characters[i][value].length;j++){
         if(this.characters[i][value][j] !== this.selectedCharacter[value][j]){
           var id = this.characters[i]["name"]
@@ -45,9 +82,9 @@ window.onload = function(){
         };
       }
     };
-
-  guessWho.printBoard();
+    guessWho.printBoard();
 };
+
 
 
 
--- js/function_for_later.js
+++ js/function_for_later.js
@@ -27,19 +27,25 @@ window.onload = function(){
   }
 
   guessWho.printBoard();
+  
 }
+  //Checker 
+  guessWho.featuresCheck = function(value, choice){
+    guessWho.dropDowns(value, choice);
+  };
 
-// guessWho.featuresCheck = function(value, choice){
-//   for(var i=0;i<this.charachter.length;i++){
-//     for(var j=0;j<this.charachter[i][value].length;j++){
-//       if(this.charachter[i][value][j] === choice){
-//         var name = this.charachter[i]["name"];
-//         document.getElementById(name).style.display = 'none';
-//       };
-//     };
-//   };
-// };
+  guessWho.dropDowns = function(value, choice){
+    for(var i=0;i<this.characters.length;i++){
+      for(var j=0;j<this.characters[i][value].length;j++){
+        if(this.characters[i][value][j] !== this.selectedCharacter[value][j]){
+          var id = this.characters[i]["name"]
+          $('#' + id).hide();
+          }
+        };
+      }
+    };
+  guessWho.printBoard();
+};
 
-// guessWho.featuresCheck("gender", "man");
 
 
